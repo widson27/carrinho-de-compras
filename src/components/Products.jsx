0151bc3d140e2e459b96a4ca8d14a42e90de9d5f
Widson Martins <131882974+widson27@users.noreply.github.com>
2024-10-09 16:29:39 -0300

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -14,7 +14,7 @@ function Products() {
       setProducts(response)
       setLoading(false)
     });
-  })
+  }, [])
 
   return (
     (loading && <Loading />) || (<section className=" max-w-[1100px] my-0 mx-auto pt-[120px] px-5 pb-[50px] grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5">
