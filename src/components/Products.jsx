d3d60066b01a0aa8a2c9825524ff6f89bf4d8d70
Widson Martins <131882974+widson27@users.noreply.github.com>
2025-11-15 15:58:57 -0300

    Refactor Products component for async data fetching
    Refactor Products component to use async function for loading products and set loading state correctly.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,21 +5,27 @@ import Loading from "./Loading";
 import AppContext from "../context/AppContext";
 
 function Products() {
-
-  const { products, setProducts, loading, setLoading } = useContext(AppContext)
+  const { products, setProducts, loading, setLoading } = useContext(AppContext);
 
   useEffect(() => {
+    const loadProducts = async () => {
+      setLoading(true);
+      const response = await fetchProducts("celular");
+      setProducts(response);
+      setLoading(false);
+    };
 
-    fetchProducts('celular').then((response) => {
-      setProducts(response)
-      setLoading(false)
-    });
-  }, [])
+    loadProducts();
+  }, []);
 
   return (
-    (loading && <Loading />) || (<section className=" max-w-[1100px] my-0 mx-auto pt-[120px] px-5 pb-[50px] grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5">
-      {products.map((product) => <ProductCard key={product.id} data={product} />)}
-    </section>)
+    (loading && <Loading />) || (
+      <section className="max-w-[1100px] my-0 mx-auto pt-[120px] px-5 pb-[50px] grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-5">
+        {products.map((product) => (
+          <ProductCard key={product.id} data={product} />
+        ))}
+      </section>
+    )
   );
 }
 
